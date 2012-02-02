12f749c7bfa12037a9fb692e86a0169805841293
Pablo <pablo@pablo-ubuntu.(none)>
2012-02-02 16:35:14 +0100

    Layout for edit menu item

--- comanda-peter/src/com/company/comanda/peter/client/UIEditMenuItem.java
+++ comanda-peter/src/com/company/comanda/peter/client/UIEditMenuItem.java
@@ -6,12 +6,20 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.DoubleBox;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.Button;
 
 public class UIEditMenuItem extends Composite {
 
     private static UIEditMenuItemUiBinder uiBinder = GWT
             .create(UIEditMenuItemUiBinder.class);
     @UiField TextBox tbName;
+    @UiField DoubleBox dbPrice;
+    @UiField FileUpload fuImageFile;
+    @UiField TextArea taDescription;
+    @UiField Button btnSaveChanges;
 
     interface UIEditMenuItemUiBinder extends UiBinder<Widget, UIEditMenuItem> {
     }
