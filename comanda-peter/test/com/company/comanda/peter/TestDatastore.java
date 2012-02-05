f9519d656fc0fd28c40b16d652ef171971fa2a18
Pablo Gil <pgilmon@gmail.com>
2012-02-05 10:58:30 +0100

    Added test project

---
+++ comanda-peter/test/com/company/comanda/peter/TestDatastore.java
@@ -0,0 +1,24 @@
+package com.company.comanda.peter;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDatastore {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
