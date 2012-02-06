97957934bb93c0242a59eadc5316e9311a90fd13
Pablo Gil <pgilmon@gmail.com>
2012-02-06 22:32:35 +0100

    Preparations for deployment

--- comanda-brian/src/com/company/comanda/brian/Constants.java
+++ comanda-brian/src/com/company/comanda/brian/Constants.java
@@ -2,5 +2,5 @@ package com.company.comanda.brian;
 
 public class Constants {
 
-    public static final String SERVER_LOCATION = "10.0.2.2:8888";
+    public static final String SERVER_LOCATION = "demo1.pgmtestapp.appspot.com";
 }
