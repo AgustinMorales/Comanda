d464747c960966e15411f2d7fadb019c3a7efa72
Pablo Gil <pgilmon@gmail.com>
2012-02-07 08:16:13 +0100

    Changed encoding

--- comanda-peter/src/com/company/comanda/peter/server/GetItemsServlet.java
+++ comanda-peter/src/com/company/comanda/peter/server/GetItemsServlet.java
@@ -35,7 +35,7 @@ public class GetItemsServlet extends HttpServlet
         List<MenuItem> items = itemsManager.getMenuItems(
                 itemsManager.getRestaurantId());
         PrintWriter out = resp.getWriter();
-        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        out.println("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>");
         out.println("<ItemList>");
         //loop through items list and print each item
         for (MenuItem i : items) 
