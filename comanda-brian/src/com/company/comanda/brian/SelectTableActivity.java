690510135aa7d4a0229031c8a57826bb20863348
Pablo Gil <pgilmon@gmail.com>
2012-02-07 21:50:52 +0100

    Scanning codes through Intents

--- comanda-brian/src/com/company/comanda/brian/SelectTableActivity.java
+++ comanda-brian/src/com/company/comanda/brian/SelectTableActivity.java
@@ -10,16 +10,22 @@ import javax.xml.parsers.SAXParserFactory;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SelectTableActivity extends Activity
 {
@@ -64,6 +70,16 @@ public class SelectTableActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_table);
+        Button read_code_button = (Button)findViewById(R.id.read_code_button);
+        read_code_button.setOnClickListener(new OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(SelectTableActivity.this);
+                integrator.initiateScan();
+                
+            }
+        });
         fetchContent();
     }
     public void fetchContent()
@@ -175,6 +191,13 @@ public class SelectTableActivity extends Activity
     //         }
     //     }    
 
-    
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+          Toast.makeText(getApplicationContext(), scanResult.getContents(), 15).show();
+        }
+        // else continue with any other code you need in the method
+        
+      }
     
 }
