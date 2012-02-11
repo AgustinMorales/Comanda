3b0f7567056506687761da4f59d46ec8f8680dc0
Pablo Gil <pgilmon@gmail.com>
2012-02-11 08:37:58 +0100

    Implemented method to get restaurant and table from code

--- comanda-peter/src/com/company/comanda/peter/server/UserManagerImpl.java
+++ comanda-peter/src/com/company/comanda/peter/server/UserManagerImpl.java
@@ -1,6 +1,7 @@
 package com.company.comanda.peter.server;
 
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 import com.company.comanda.peter.server.model.MenuItem;
@@ -10,6 +11,7 @@ import com.company.comanda.peter.server.model.Table;
 import com.company.comanda.peter.server.model.User;
 import com.company.comanda.peter.shared.OrderState;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.NotFoundException;
 import com.googlecode.objectify.Objectify;
 
 public class UserManagerImpl implements UserManager {
@@ -17,11 +19,11 @@ public class UserManagerImpl implements UserManager {
     private static final Logger log = Logger.
             getLogger(UserManagerImpl.class.getName());
     private Objectify ofy;
-    
+
     public UserManagerImpl(Objectify ofy){
         this.ofy = ofy;
     }
-    
+
     @Override
     public void placeOrder(long userId, String password, long restaurantId,
             long menuItemId, long tableId) {
@@ -46,16 +48,47 @@ public class UserManagerImpl implements UserManager {
 
     @Override
     public CodifiedData getData(String code) {
+        CodifiedData result = null;
+
         int totalLength = code.length();
         int restaurantLength = totalLength - 
                 (Table.TABLE_CODE_ID_PART_WIDTH +
                         Table.TABLE_CODE_RANDOM_PART_WIDTH);
-        
+
         long restaurantId = Long.parseLong(
                 code.substring(0, restaurantLength));
-        String 
-        
-        return null;
+        String table_code = code.substring(restaurantLength);
+
+        Key<Restaurant> restaurantKey = 
+                new Key<Restaurant>(Restaurant.class, restaurantId);
+
+        List<Table> tables = ofy.query(Table.class).
+                filter("code", table_code).
+                ancestor(restaurantKey).list();
+
+        if(tables.size() == 1){
+            try{
+                Restaurant restaurant = ofy.get(restaurantKey);
+                Table table = tables.get(0);
+                result = new CodifiedData();
+                result.restaurant = restaurant;
+                result.table = table;
+            }
+            catch(NotFoundException e){
+                log.info("Could not find restaurant. Code='" + 
+                        code + "'");
+            }
+        }
+        else{
+            if(tables.size() > 1){
+                throw new IllegalStateException(
+                        "More than one table with the same code. " +
+                                "Restaurant: '" + restaurantId + 
+                                "'. Code: '" + table_code + "'");
+            }
+        }
+
+        return result;
     }
 
 }
