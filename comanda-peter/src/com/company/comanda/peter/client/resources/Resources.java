b95375cf7c012ca76d2e9a09640849536e8ac2b4
Pablo Gil <pgilmon@gmail.com>
2012-02-25 12:08:45 +0100

    Nwo with banner

---
+++ comanda-peter/src/com/company/comanda/peter/client/resources/Resources.java
@@ -0,0 +1,13 @@
+package com.company.comanda.peter.client.resources;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * Resources used by the entire application.
+ */
+public interface Resources extends ClientBundle {
+
+  @Source("images/banner.png")
+  ImageResource banner();
+}
