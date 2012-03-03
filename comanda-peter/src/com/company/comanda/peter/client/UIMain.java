15ede5b8687ff1aa5e05714a281ae2f2872d6609
Pablo Gil <pgilmon@gmail.com>
2012-03-03 16:33:27 +0100

    Restaurant tag

--- comanda-peter/src/com/company/comanda/peter/client/UIMain.java
+++ comanda-peter/src/com/company/comanda/peter/client/UIMain.java
@@ -24,7 +24,7 @@ public class UIMain extends Composite {
 
     public UIMain(String restaurantName) {
         initWidget(uiBinder.createAndBindUi(this));
-        lblRestaurantName.setText(restaurantName);
+        lblRestaurantName.setText("Restaurante: " + restaurantName);
     }
 
     @UiHandler("mainTabPanel")
