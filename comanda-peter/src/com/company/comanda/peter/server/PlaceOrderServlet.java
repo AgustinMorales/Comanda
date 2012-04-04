fd05f6403eedd6a3b204770fa843cf45da167622
Pablo Gil <pgilmon@gmail.com>
2012-04-04 17:09:31 +0200

    Handling exceptions on PlaceorderServlet

--- comanda-peter/src/com/company/comanda/peter/server/PlaceOrderServlet.java
+++ comanda-peter/src/com/company/comanda/peter/server/PlaceOrderServlet.java
@@ -28,65 +28,71 @@ public class PlaceOrderServlet extends HttpServlet
      * 
      */
     private static final long serialVersionUID = 7406683513326724866L;
-    
+
     private static final Logger log = LoggerFactory.getLogger(PlaceOrderServlet.class);
-    
+
     private UserManager userManager;
 
     @Inject
     public PlaceOrderServlet(UserManager manager){
         this.userManager = manager;
     }
-    
+
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
         doPost(req, resp);
     }
-    
+
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException
     {
-        ServletHelper.logParameters(req, log);
-        String menuItemIdsString = req.getParameter(PARAM_ITEM_IDS);
-        String tableIdString = req.getParameter(PARAM_TABLE_ID);
-        String address = req.getParameter(PARAM_ADDRESS);
-//        String menuItemComments = req.getParameter(PARAM_MENU_ITEM_COMMENTS);
-        String billKeyString = req.getParameter(PARAM_BILL_KEY_STRING);
-        String qualifierIndexesString = req.getParameter(PARAM_QUALIFIERS);
-        String comments = req.getParameter(PARAM_COMMENTS);
-        Long tableId = null;
-        if(tableIdString.length() > 0){
-            tableId = Long.parseLong(req.getParameter(PARAM_TABLE_ID));
-        }
-        long userId = Long.parseLong(req.getParameter(PARAM_USER_ID));
-        String password = req.getParameter(PARAM_PASSWORD);
-        long restaurantId = Long.parseLong(
-                req.getParameter(PARAM_RESTAURANT_ID));
-        
-        //FIXME: What happens in case of error?
-        String[] items = menuItemIdsString.split(":");
-        String[] indexes = qualifierIndexesString.split(":");
-        List<Long> menuItemIds = new ArrayList<Long>(items.length);
-        List<String> menuItemComments = new ArrayList<String>(items.length);
-        List<Integer> qualifierIndexes = new ArrayList<Integer>(items.length);
-        for (int i=0;i<items.length;i++){
-        	String item = items[i];
-        	String qualifierIndex = indexes[i];
-            log.debug("Adding item ID: {}", item);
-            long menuItemId = Long.parseLong(item);
-            menuItemIds.add(menuItemId);
-            menuItemComments.add("");
-            qualifierIndexes.add(Integer.parseInt(qualifierIndex));
-        }
-        //FIXME: User real qualifierIndex
-        userManager.placeOrder(userId, password, 
-                restaurantId, menuItemIds, 
-                qualifierIndexes,
-                menuItemComments,
-                address,
-                tableId,
-                comments,
-                tableId==null?BillType.DELIVERY:BillType.IN_RESTAURANT,
-                        billKeyString);
         PrintWriter out = ServletHelper.getXmlWriter(resp);
-        out.println(enclose(RESULT, "" + true));
+        try{
+            ServletHelper.logParameters(req, log);
+            String menuItemIdsString = req.getParameter(PARAM_ITEM_IDS);
+            String tableIdString = req.getParameter(PARAM_TABLE_ID);
+            String address = req.getParameter(PARAM_ADDRESS);
+            //        String menuItemComments = req.getParameter(PARAM_MENU_ITEM_COMMENTS);
+            String billKeyString = req.getParameter(PARAM_BILL_KEY_STRING);
+            String qualifierIndexesString = req.getParameter(PARAM_QUALIFIERS);
+            String comments = req.getParameter(PARAM_COMMENTS);
+            Long tableId = null;
+            if(tableIdString.length() > 0){
+                tableId = Long.parseLong(req.getParameter(PARAM_TABLE_ID));
+            }
+            long userId = Long.parseLong(req.getParameter(PARAM_USER_ID));
+            String password = req.getParameter(PARAM_PASSWORD);
+            long restaurantId = Long.parseLong(
+                    req.getParameter(PARAM_RESTAURANT_ID));
+
+            //FIXME: What happens in case of error?
+            String[] items = menuItemIdsString.split(":");
+            String[] indexes = qualifierIndexesString.split(":");
+            List<Long> menuItemIds = new ArrayList<Long>(items.length);
+            List<String> menuItemComments = new ArrayList<String>(items.length);
+            List<Integer> qualifierIndexes = new ArrayList<Integer>(items.length);
+            for (int i=0;i<items.length;i++){
+                String item = items[i];
+                String qualifierIndex = indexes[i];
+                log.debug("Adding item ID: {}", item);
+                long menuItemId = Long.parseLong(item);
+                menuItemIds.add(menuItemId);
+                menuItemComments.add("");
+                qualifierIndexes.add(Integer.parseInt(qualifierIndex));
+            }
+            //FIXME: User real qualifierIndex
+            userManager.placeOrder(userId, password, 
+                    restaurantId, menuItemIds, 
+                    qualifierIndexes,
+                    menuItemComments,
+                    address,
+                    tableId,
+                    comments,
+                    tableId==null?BillType.DELIVERY:BillType.IN_RESTAURANT,
+                            billKeyString);
+            out.println(enclose(RESULT, "" + true));
+        }
+        catch(Exception e){
+            log.error("Exception while placing order", e);
+            out.println(enclose(RESULT, "" + false));
+        }
     }
 }
